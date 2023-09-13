33415a3a3169e500a11ecad9aaf0982e695190dd
haikalirhamna <haikalirhamna15@gmail.com>
2023-09-13 14:07:02 +0700

    init

---
+++ src/components/CounterApp.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import CounterDisplay from "./CounterDisplay";
+import IncreaseButton from "./IncreaseButton";
+import ResetButton from "./ResetButton";
+
+class CounterApp extends React.Component {
+    constructor(props) {
+      super(props);
+   
+      // inisialisasi nilai count di dalam state
+      this.state = {
+        count: 0
+      };
+
+      this.onIncreaseEventHandler = this.onIncreaseEventHandler.bind(this);
+      this.onResetEventHandler = this.onResetEventHandler.bind(this);
+    }
+   
+    onIncreaseEventHandler() {
+      this.setState((previousState) => {
+        return {
+          count: previousState.count + 1
+        };
+      });
+    }
+    onResetEventHandler() {
+      this.setState(() => {
+        return {
+          count: 0
+        };
+      });
+    }
+   
+    render() {
+      return (
+        <div>
+          <IncreaseButton increase={this.onIncreaseEventHandler} />
+          <CounterDisplay count={this.state.count} />
+          <ResetButton reset={this.onResetEventHandler} />
+        </div>
+      );
+    }
+  }
+
+export default CounterApp;
